9c7c6c9808f3f4ad6f6eeeae67006588916a7679
Lysandros Nikolaou <lisandrosnik@gmail.com>
2025-02-05 00:12:19 +0100

    Make C extension tests optional / refactor filtering of tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,6 @@
-import pathlib
 import pytest
 
 from . import setup_modules
-from . import utils
 
 
 def pytest_configure(config):
@@ -44,35 +42,6 @@ def _ext(request):
     if request.node.get_closest_marker("needs_resolver_helpers") is not None:
         resolver_helpers = True
 
-    setup(constructor_helpers, structure_helpers, resolver_helpers)
+    setup(request.node, constructor_helpers, structure_helpers, resolver_helpers)
     yield
     teardown(constructor_helpers, structure_helpers, resolver_helpers)
-
-
-@pytest.fixture(autouse=True)
-def filter_python_only_tests(request, _ext):
-    node = request.node
-    if setup_modules.RUNNING_C_EXT and node.get_closest_marker("python_only") is not None:
-        pytest.skip("Skipping python-only when running cext")
-
-    if not setup_modules.RUNNING_C_EXT and node.get_closest_marker("c_only") is not None:
-        pytest.skip("Skipping c-only when running pyext")
-
-    if not setup_modules.RUNNING_C_EXT:
-        return
-
-    try:
-        callspec = node.callspec
-    except AttributeError:
-        return
-
-    file = None
-    for v in callspec.params.values():
-        if isinstance(v, pathlib.Path):
-            file = v
-            break
-    else:
-        return
-    data_files = utils.all_data_files()
-    if ".skip-ext" in data_files[file.stem]:
-        pytest.skip("Skipping .skip-ext when running cext")
--- tests/setup_modules.py
+++ tests/setup_modules.py
@@ -1,11 +1,13 @@
+import pathlib
+import pytest
+
 import yaml
 
+from . import utils
 from .constructor_helpers import make_constructor_helpers
 from .resolver_helpers import make_resolver_helpers
 from .structure_helpers import make_structure_helpers
 
-
-RUNNING_C_EXT = False
 
 yaml.PyBaseLoader = yaml.BaseLoader
 yaml.PySafeLoader = yaml.SafeLoader
@@ -14,65 +16,66 @@ yaml.PyBaseDumper = yaml.BaseDumper
 yaml.PySafeDumper = yaml.SafeDumper
 yaml.PyDumper = yaml.Dumper
 
-old_scan = yaml.scan
-def new_scan(stream, Loader=yaml.CLoader):
-    return old_scan(stream, Loader)
+if yaml.__with_libyaml__:
+    old_scan = yaml.scan
+    def new_scan(stream, Loader=yaml.CLoader):
+        return old_scan(stream, Loader)
 
-old_parse = yaml.parse
-def new_parse(stream, Loader=yaml.CLoader):
-    return old_parse(stream, Loader)
+    old_parse = yaml.parse
+    def new_parse(stream, Loader=yaml.CLoader):
+        return old_parse(stream, Loader)
 
-old_compose = yaml.compose
-def new_compose(stream, Loader=yaml.CLoader):
-    return old_compose(stream, Loader)
+    old_compose = yaml.compose
+    def new_compose(stream, Loader=yaml.CLoader):
+        return old_compose(stream, Loader)
 
-old_compose_all = yaml.compose_all
-def new_compose_all(stream, Loader=yaml.CLoader):
-    return old_compose_all(stream, Loader)
+    old_compose_all = yaml.compose_all
+    def new_compose_all(stream, Loader=yaml.CLoader):
+        return old_compose_all(stream, Loader)
 
-old_load = yaml.load
-def new_load(stream, Loader=yaml.CLoader):
-    return old_load(stream, Loader)
+    old_load = yaml.load
+    def new_load(stream, Loader=yaml.CLoader):
+        return old_load(stream, Loader)
 
-old_load_all = yaml.load_all
-def new_load_all(stream, Loader=yaml.CLoader):
-    return old_load_all(stream, Loader)
+    old_load_all = yaml.load_all
+    def new_load_all(stream, Loader=yaml.CLoader):
+        return old_load_all(stream, Loader)
 
-old_safe_load = yaml.safe_load
-def new_safe_load(stream):
-    return old_load(stream, yaml.CSafeLoader)
+    old_safe_load = yaml.safe_load
+    def new_safe_load(stream):
+        return old_load(stream, yaml.CSafeLoader)
 
-old_safe_load_all = yaml.safe_load_all
-def new_safe_load_all(stream):
-    return old_load_all(stream, yaml.CSafeLoader)
+    old_safe_load_all = yaml.safe_load_all
+    def new_safe_load_all(stream):
+        return old_load_all(stream, yaml.CSafeLoader)
 
-old_emit = yaml.emit
-def new_emit(events, stream=None, Dumper=yaml.CDumper, **kwds):
-    return old_emit(events, stream, Dumper, **kwds)
+    old_emit = yaml.emit
+    def new_emit(events, stream=None, Dumper=yaml.CDumper, **kwds):
+        return old_emit(events, stream, Dumper, **kwds)
 
-old_serialize = yaml.serialize
-def new_serialize(node, stream, Dumper=yaml.CDumper, **kwds):
-    return old_serialize(node, stream, Dumper, **kwds)
+    old_serialize = yaml.serialize
+    def new_serialize(node, stream, Dumper=yaml.CDumper, **kwds):
+        return old_serialize(node, stream, Dumper, **kwds)
 
-old_serialize_all = yaml.serialize_all
-def new_serialize_all(nodes, stream=None, Dumper=yaml.CDumper, **kwds):
-    return old_serialize_all(nodes, stream, Dumper, **kwds)
+    old_serialize_all = yaml.serialize_all
+    def new_serialize_all(nodes, stream=None, Dumper=yaml.CDumper, **kwds):
+        return old_serialize_all(nodes, stream, Dumper, **kwds)
 
-old_dump = yaml.dump
-def new_dump(data, stream=None, Dumper=yaml.CDumper, **kwds):
-    return old_dump(data, stream, Dumper, **kwds)
+    old_dump = yaml.dump
+    def new_dump(data, stream=None, Dumper=yaml.CDumper, **kwds):
+        return old_dump(data, stream, Dumper, **kwds)
 
-old_dump_all = yaml.dump_all
-def new_dump_all(documents, stream=None, Dumper=yaml.CDumper, **kwds):
-    return old_dump_all(documents, stream, Dumper, **kwds)
+    old_dump_all = yaml.dump_all
+    def new_dump_all(documents, stream=None, Dumper=yaml.CDumper, **kwds):
+        return old_dump_all(documents, stream, Dumper, **kwds)
 
-old_safe_dump = yaml.safe_dump
-def new_safe_dump(data, stream=None, **kwds):
-    return old_dump(data, stream, yaml.CSafeDumper, **kwds)
+    old_safe_dump = yaml.safe_dump
+    def new_safe_dump(data, stream=None, **kwds):
+        return old_dump(data, stream, yaml.CSafeDumper, **kwds)
 
-old_safe_dump_all = yaml.safe_dump_all
-def new_safe_dump_all(documents, stream=None, **kwds):
-    return old_dump_all(documents, stream, yaml.CSafeDumper, **kwds)
+    old_safe_dump_all = yaml.safe_dump_all
+    def new_safe_dump_all(documents, stream=None, **kwds):
+        return old_dump_all(documents, stream, yaml.CSafeDumper, **kwds)
 
 
 def setup_objects(constructor_helpers, structure_helpers, resolver_helpers):
@@ -84,18 +87,39 @@ def setup_objects(constructor_helpers, structure_helpers, resolver_helpers):
         make_resolver_helpers()
 
 
-def setup_pyext(constructor_helpers, structure_helpers, resolver_helpers):
-    global RUNNING_C_EXT
+def setup_pyext(node, constructor_helpers, structure_helpers, resolver_helpers):
+    if node.get_closest_marker("c_only") is not None:
+        pytest.skip("Skipping c-only when running pyext")
+
     setup_objects(constructor_helpers, structure_helpers, resolver_helpers)
-    RUNNING_C_EXT = False
 
 
 def teardown_pyext(constructor_helpers, structure_helpers, resolver_helpers):
     pass
 
 
-def setup_cext(constructor_helpers, structure_helpers, resolver_helpers):
-    global RUNNING_C_EXT
+def setup_cext(node, constructor_helpers, structure_helpers, resolver_helpers):
+    if not yaml.__with_libyaml__:
+        pytest.skip("cext tests not run when C extension is not built")
+
+    if node.get_closest_marker("python_only") is not None:
+        pytest.skip("Skipping python-only when running cext")
+
+    try:
+        callspec = node.callspec
+    except AttributeError:
+        pass
+    else:
+        file = None
+        for v in callspec.params.values():
+            if isinstance(v, pathlib.Path):
+                file = v
+                break
+        if file is not None:
+            data_files = utils.all_data_files()
+            if ".skip-ext" in data_files[file.stem]:
+                pytest.skip("Skipping .skip-ext when running cext")
+
     yaml.BaseLoader = yaml.CBaseLoader
     yaml.SafeLoader = yaml.CSafeLoader
     yaml.Loader = yaml.CLoader
@@ -118,7 +142,6 @@ def setup_cext(constructor_helpers, structure_helpers, resolver_helpers):
     yaml.safe_dump = new_safe_dump
     yaml.safe_dump_all = new_safe_dump_all
     setup_objects(constructor_helpers, structure_helpers, resolver_helpers)
-    RUNNING_C_EXT = True
 
 
 def teardown_cext(constructor_helpers, structure_helpers, resolver_helpers):
