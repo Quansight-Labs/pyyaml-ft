54bfcbe84c2107c90b6b1344c4f0b095c5bfc7c1
Lysandros Nikolaou <lisandrosnik@gmail.com>
2025-05-26 19:58:32 +0200

    Upgrade to Cython 3.1 final

--- packaging/build/cibw_before_build.sh
+++
@@ -1,5 +0,0 @@
-FREE_THREADED_BUILD="$(python -c"import sysconfig; print(bool(sysconfig.get_config_var('Py_GIL_DISABLED')))")"
-if [[ $FREE_THREADED_BUILD == "True" ]]; then
-    python -m pip install -U pip
-    python -m pip install cython==3.1.0rc1 setuptools wheel
-fi
